230aa035dde9622dd0f3fdd652b8dbeaa3fd884d
Suraj Vhatakar <surajvhatakar@gmail.com>
2026-09-22 20:17:05 +0000

    Add Prometheus monitoring support

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
@@ -17,6 +18,11 @@ def health():
     return jsonify({
         "status": "healthy"
     })
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
 if __name__ == "__main__":
